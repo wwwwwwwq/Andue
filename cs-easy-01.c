d4dbd5c18887a42cb7b87dea5cbb548f51d1518e
Andue <14904096+andue@user.noreply.gitee.com>
2024-09-21 12:19:59 +0800

    Hello

--- cs-easy-01.c
+++ cs-easy-01.c
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 # include<stdio.h>
 // 辗转相除法就搞定了喵
 int main()
@@ -14,20 +13,3 @@ int main()
 	 printf("%d",m);
 	 return 0;
 }
-=======
-# include<stdio.h>
-// 辗转相除法就搞定了喵
-int main()
-{
-	int m,n,t=1;
-	scanf("%d %d",&m,&n);
-	while(t)
-	{
-		t=m%n;
-		m=n;
-		n=t;
-	 } 
-	 printf("%d",m);
-	 return 0;
-}
->>>>>>> 11db3e23b77b846f31b7f7047f8a5142f25522b6
